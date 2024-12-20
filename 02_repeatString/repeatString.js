32138b513f2865e4a4f9054003e9d7f28c2b1f4d
dbelett98 <dbelett@outlook.com>
2024-12-20 21:21:46 +0800

    added code to repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+if (num < 0) {
+    return 'ERROR';
+}  else {
+    // create array filled with repeated string
+    let result = [];
+    for (let i=0; i<num; i++) {
+        result.push(str)
+    }
 
-};
+    //join array into single string
+    return result.join('');
+    }
+}
+;
 
 // Do not edit below this line
 module.exports = repeatString;
